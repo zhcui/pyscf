a460fba96ead9add61f85e739671e3b77a4a8eef
fishjojo <zhangxing.nju@gmail.com>
2020-01-13 22:10:53 -0800

    Revert "Merge remote-tracking branch 'origin/master' into kpoint_symm"
    This reverts commit ebfce6621436e54ebfcec6539b01551087e5c0e1, reversing
    changes made to 19e997572ca578e7ae5631ce95d8167f2424d32e.

--- examples/qmmm/22-with_solvent.py
+++ examples/qmmm/22-with_solvent.py
@@ -9,7 +9,6 @@ QM/MM charges + implicit solvent model
 
 import numpy
 from pyscf import gto, scf, qmmm, solvent
-from pyscf.data import radii
 
 mol = gto.M(atom='''
 C        0.000000    0.000000             -0.542500
@@ -21,35 +20,7 @@ H        0.000000   -0.9353074360871938   -1.082500
 
 numpy.random.seed(1)
 coords = numpy.random.random((5,3)) * 10
-charges = (numpy.arange(5) + 1.) * .1
-
-class QMMMMole(gto.Mole):
-    def __init__(self, mol, coords, charges):
-        mm_atoms = [('C', c) for c in coords]
-        mm_mol = gto.M(atom=mm_atoms, basis={})
-        self.qm_mol = mol
-        self.mm_mol = mm_mol
-        self.mm_charges = charges
-        qmmm_mol = mol + mm_mol
-        self.__dict__.update(qmmm_mol.__dict__)
-
-    def atom_charge(self, atom_id):
-        if atom_id < self.qm_mol.natm:
-            return self.qm_mol.atom_charge(atom_id)
-        else:
-            return self.mm_charges[atom_id-self.qm_mol.natm]
-
-    def atom_charges(self):
-        return numpy.append(self.qm_mol.atom_charges(), self.mm_charges)
-
-# Make a giant system include both QM and MM particles
-qmmm_mol = QMMMMole(mol, coords, charges)
-
-# The solvent model is based on the giant system
-sol = solvent.DDCOSMO(qmmm_mol)
-
-# According to Lipparini's suggestion in issue #446
-sol.radii_table = radii.VDW
+charges = (numpy.arange(5) + 1.) * -.1
 
 #
 # The order to apply solvent model and QM/MM charges does not affect results
@@ -58,13 +29,13 @@ sol.radii_table = radii.VDW
 #
 mf = scf.RHF(mol)
 mf = qmmm.mm_charge(mf, coords, charges)
-mf = solvent.ddCOSMO(mf, sol)
+mf = solvent.ddCOSMO(mf)
 mf.run()
 
 #
 # QMMM-ddCOSMO-SCF
 #
 mf = scf.RHF(mol)
-mf = solvent.ddCOSMO(mf, sol)
+mf = solvent.ddCOSMO(mf)
 mf = qmmm.mm_charge(mf, coords, charges)
 mf.run()
